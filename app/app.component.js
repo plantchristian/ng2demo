25b29b52cec9a520a440fa42862f950eb2e381c9
plantchristian <christian@plantcph.dk>
2016-06-16 09:18:49 +0200

    add files

---
+++ app/app.component.js
@@ -0,0 +1,9 @@
+"use strict";
+var AppComponent = (function () {
+    function AppComponent() {
+        this.title = "Some Title";
+    }
+    return AppComponent;
+}());
+exports.AppComponent = AppComponent;
+//# sourceMappingURL=app.component.js.map
